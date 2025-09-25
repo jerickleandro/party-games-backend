ed3a304bb0991e8290b064c29ac6683cb8c27a3b
Erick Leandro <j.erickleandro@gmail.com>
2025-09-25 22:39:20 +0000

    refactor: restart project

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import { config } from './config';
-import { createHttpServer } from './server/http';
-
-
-const { server } = createHttpServer();
-server.listen(config.port, () => console.log(`[server] on :${config.port}`));
